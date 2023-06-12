cac03d77bc954872553b31b3485eea5e40c887d9
shamssab <133707567+shamssab@users.noreply.github.com>
2023-06-11 21:53:05 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
-	}
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial();
+        System.out.println(p.evaluate(3));
+
+        double[] c1 = {6, 0, 0, 5};
+        int[] e1 = {0, 1, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double[] c2 = {0, -2, 0, 0, -9};
+        int[] e2 = {0, 1, 3, 4};
+        Polynomial p2 = new Polynomial(c2, e2);
+
+        Polynomial s = p1.add(p2);
+        System.out.println("s(0.1) = " + s.evaluate(0.1));
+
+        if (s.hasRoot(1))
+            System.out.println("1 is a root of s");
+        else
+            System.out.println("1 is not a root of s");
+
+        Polynomial product = p1.multiply(p2);
+        System.out.println("Product: " + product.evaluate(2));
+
+        try {
+            Polynomial polynomialFromFile = new Polynomial(new File("polynomial.txt"));
+            System.out.println("Polynomial from file: " + polynomialFromFile.evaluate(2));
+            polynomialFromFile.saveToFile("saved_polynomial.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
 }
